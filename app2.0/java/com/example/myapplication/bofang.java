3877aecf83c0b3d7514822b7b2853dac819018ef
jchxch <70745797+jchxch@users.noreply.github.com>
2020-12-06 18:44:12 +0800

    搜索功能拓展

---
+++ app2.0/java/com/example/myapplication/bofang.java
@@ -0,0 +1,63 @@
+package com.example.myapplication;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
+
+public class bofang extends AppCompatActivity {
+    EditText urlst,name1;
+    Button click;
+    MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.online);
+        urlst=findViewById(R.id.urlst);
+        name1=findViewById(R.id.name);
+        click=findViewById(R.id.click);
+        click.setText("播放");
+        String id=getIntent().getStringExtra("id");
+        String name=getIntent().getStringExtra("name");
+        urlst.setText("相似歌曲id为"+id);
+        name1.setText("相似歌曲名称为"+name);
+        String mp3_url=getIntent().getStringExtra("mp3_url");
+        if(mediaPlayer==null)
+        {
+            mediaPlayer = new MediaPlayer();
+            try {
+                mediaPlayer.setDataSource(mp3_url);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            mediaPlayer.prepareAsync();
+        }
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(mediaPlayer!=null)
+                {
+
+                    if(mediaPlayer.isPlaying())
+                    {
+                        mediaPlayer.pause();
+                        click.setText("继续");
+                    }
+                    else
+                    {
+                        mediaPlayer.start();
+                        click.setText("暂停");
+                    }
+                }
+            }
+        });
+    }
+}
+
---
+++ app2.0/java/com/example/myapplication/online.java
@@ -0,0 +1,122 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
+
+public class online extends AppCompatActivity {
+    private Button mBtReturn;
+    private  Button clickbtn ;
+    private Button button1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bf);
+        final EditText editText1 =(EditText) findViewById (R.id.ed);
+        final EditText editText2 =(EditText) findViewById (R.id.search_box);
+        button1 = (Button) findViewById(R.id.search_box_a);
+        button1.setOnClickListener(new View.OnClickListener() {
+                                       @Override
+                                       public void onClick(View v) {
+                                           String mp3_url=null;
+                                           StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                                                   .detectDiskReads().detectDiskWrites().detectNetwork()
+                                                   .penaltyLog().build());
+                                           StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                                                   .detectLeakedSqlLiteObjects().detectLeakedClosableObjects()
+                                                   .penaltyLog().penaltyDeath().build());
+                                           String c = editText2.getText().toString();
+                                           httpurlconnection a= new httpurlconnection();//获取歌曲id的类
+                                           String text = null;
+                                           try {
+                                               text=a.httpurlconnection(c);//获取歌曲id
+                                           } catch (IOException e) {
+                                               e.printStackTrace();
+                                           }
+                                           getmp3 mp3 = new getmp3();//获取id歌曲的url的类
+                                           try {
+                                               mp3_url= mp3.get_url(text);//获取id歌曲的url
+                                           } catch (IOException e) {
+                                               e.printStackTrace();
+                                           }
+                                           Intent intent = new Intent(online.this,bofang.class);
+                                           //传入参数，账号，密码   //username、password是跳转目标页面获取它的参数   UserName、UserPassword是要传入的参数
+                                           intent.putExtra("mp3_url",mp3_url);
+                                           intent.putExtra("id",text);
+                                           intent.putExtra("name",c);
+                                           startActivity(intent);
+                                       }
+
+                                   }
+        );
+        clickbtn =  (Button) findViewById(R.id.a);
+        clickbtn.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View v)  {
+                                            //获取MP3url
+                                            String mp3_url=null;
+                                            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                                                    .detectDiskReads().detectDiskWrites().detectNetwork()
+                                                    .penaltyLog().build());
+                                            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                                                    .detectLeakedSqlLiteObjects().detectLeakedClosableObjects()
+                                                    .penaltyLog().penaltyDeath().build());
+                                            String c = editText1.getText().toString();
+                                            httpurlconnection a= new httpurlconnection();//获取歌曲id的类
+                                            String text = null;
+                                            try {
+                                                text=a.httpurlconnection(c);//获取歌曲id
+                                            } catch (IOException e) {
+                                                e.printStackTrace();
+                                            }
+                                            //String str = text.substring(2,8);
+                                            Toast toast=Toast.makeText(getApplicationContext(), "歌曲ID："+text, Toast.LENGTH_SHORT);
+                                            toast.show();
+                                            compare cp = new compare();//获取推荐歌曲的id的类
+                                            try {
+                                                String[] t = cp.find(text);//获取推荐歌曲id
+                                                toast =Toast.makeText(getApplicationContext(), "推荐歌曲ID："+t[0], Toast.LENGTH_SHORT);
+                                               
+                                                toast =Toast.makeText(getApplicationContext(), "推荐歌曲名称："+t[1], Toast.LENGTH_SHORT);
+
+                                                getmp3 mp3 = new getmp3();//获取id歌曲的url的类
+                                                mp3_url= mp3.get_url(t[0]);//获取id歌曲的url
+                                                toast =Toast.makeText(getApplicationContext(), "推荐歌曲url："+mp3_url, Toast.LENGTH_SHORT);
+
+                                                Intent intent = new Intent(online.this,bofang.class);
+                                                //传入参数，账号，密码   //username、password是跳转目标页面获取它的参数   UserName、UserPassword是要传入的参数
+                                                intent.putExtra("mp3_url",mp3_url);
+                                                intent.putExtra("id",t[0]);
+                                                intent.putExtra("name",t[1]);
+                                                startActivity(intent);
+                                            } catch (IOException e) {
+                                                e.printStackTrace();
+                                                toast =Toast.makeText(getApplicationContext(), "错误："+e.toString(), Toast.LENGTH_SHORT);
+                                                toast.show();
+                                            }
+
+
+
+                                            //播放
+
+
+
+
+                                        }
+
+
+
+                                    }
+
+        );
+    }
+
+}
